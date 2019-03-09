006127db7bb0d7961722562a3fffbdbdb65d3ace
Konstantin Münster <konstantin.muenster@mail.com>
2019-03-09 22:23:15 +0100

    Updates

---
+++ precache-manifest.c6d35f692941663a1019cb4bc6e0adfa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "68089f4ad76df98dd4a54d12e23cccc0",
+    "url": "/react-todo-list/static/media/close.68089f4a.svg"
+  },
+  {
+    "revision": "119e20bf9c9d78fa8c45dd4a2eb4956c",
+    "url": "/react-todo-list/static/media/circle-done.119e20bf.svg"
+  },
+  {
+    "revision": "826537cf3c78f08e0dccc746f12f7b7c",
+    "url": "/react-todo-list/static/media/circle-blank.826537cf.svg"
+  },
+  {
+    "revision": "14e46cb4c53468dced63",
+    "url": "/react-todo-list/static/js/runtime~main.d51d0b1e.js"
+  },
+  {
+    "revision": "dbb1525a98706b77d6dd",
+    "url": "/react-todo-list/static/js/main.1b92dda9.chunk.js"
+  },
+  {
+    "revision": "c94871e44ee6febb7383",
+    "url": "/react-todo-list/static/js/2.b780763f.chunk.js"
+  },
+  {
+    "revision": "dbb1525a98706b77d6dd",
+    "url": "/react-todo-list/static/css/main.074d23fa.chunk.css"
+  },
+  {
+    "revision": "32a4359ec63f8ec9add190c7b8f6730a",
+    "url": "/react-todo-list/index.html"
+  }
+];
